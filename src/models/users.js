dcf306e376260899348a5a76a1316ad605cada8a
bimaap <let.bimaap@gmail.com>
2022-06-28 15:59:50 +0700

    feat: add users models

---
+++ src/models/users.js
@@ -0,0 +1,15 @@
+const db = require('../helpers/db')
+
+exports.getDataQuery = async (sql, cb) =>{
+    try{
+        const result = await db.query(sql)
+        if(result.rowCount){
+            cb(true, result.rows)
+        }else{
+            cb(false, 'Data tidak ditemukan')
+        }
+    }catch(err){
+        console.log(err);
+        cb(false, err.message)
+    }
+}
