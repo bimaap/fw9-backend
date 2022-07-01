e841d15277ecf7b784453619c24e2fbade97b6b5
bimaap <let.bimaap@gmail.com>
2022-07-01 13:52:20 +0700

    fix: implementation bcrypt in password user

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,11 +1,16 @@
 const users = require('express').Router()
 const userController = require('../controllers/users')
 const { check } = require('express-validator');
+const bcrypt = require('bcrypt')
 
 const validationUser = [
     check('username').isLength({min:4}).withMessage('Username length min 4'),
     check('email').isEmail().withMessage('Incorrect email structure'),
-    check('password').isLength({min:6}).withMessage('Password length min 6'),
+    check('password')
+        .isLength({min:6}).withMessage('Password length min 6')
+        .customSanitizer(async (pass)=>{
+            return await bcrypt.hash(pass, 1)
+        }),
     check('pin').isLength({min:6, max:6}).withMessage('Pin length must be 6')
 ]
 
